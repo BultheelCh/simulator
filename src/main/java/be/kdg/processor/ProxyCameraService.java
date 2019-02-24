7eb4f4f36b3b9896f7e165da75c3f7fa1c2044c7
BultheelCh <christophe.bultheel@student.kdg.be>
2019-02-24 19:09:32 +0100

    initial import

--- src/main/java/be/kdg/processor/ProxyCameraService.java
+++ src/main/java/be/kdg/processor/ProxyCameraService.java
@@ -3,9 +3,6 @@ package be.kdg.processor;
 import be.kdg.sa.services.CameraServiceProxy;
 import org.springframework.stereotype.Component;
 
-import javax.json.Json;
-import javax.json.JsonReader;
-import javax.xml.bind.JAXB;
 import java.io.IOException;
 
 @Component
@@ -24,9 +21,9 @@ public class ProxyCameraService implements CameraService{
 
 
     private void ConvertResultCameraService(String result){
-        JsonReader
-        Json g = new Json( );
-        Camera camera = fo g.fromJson(result, Camera.class);
+       // JsonReader
+        //Json g = new Json( );
+        //Camera camera = fo g.fromJson(result, Camera.class);
 
 
     }
